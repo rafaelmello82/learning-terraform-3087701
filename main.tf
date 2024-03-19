6d92a5a95234618af08372a4fde4df2a6face3e4
rafaelmello82 <64922281+rafaelmello82@users.noreply.github.com>
2024-03-19 18:03:45 +0000

    fix spaces

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "app_ami" {
+ezdrgxvw4asezdsfcdata "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids =[aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraform"
