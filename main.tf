78492be22be82ae2c697a6ad9260e785d3ddb5b8
rafaelmello82 <64922281+rafaelmello82@users.noreply.github.com>
2024-03-22 15:28:23 +0000

    fix listeners

--- main.tf
+++ main.tf
@@ -63,17 +63,15 @@ module "alb" {
     }
   ]
 }
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
     }
-  ]
   tags = {
     Environment = "dev"
   }
-
+  }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
